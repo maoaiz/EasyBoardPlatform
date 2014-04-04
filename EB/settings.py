886b63bd167e5e9c680fa28c9087abedfd90aa74
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-04-04 18:40:24 -0500

    Update databases var

--- EB/settings.py
+++ EB/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_NAME = "EasyBoard"
-PRINCIPAL_DOMAIN = "easyboard.co"
+PRINCIPAL_DOMAIN = "easyboard.com.co"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['easyboard.com.co']
 
 
 # Application definition
@@ -62,12 +62,15 @@ WSGI_APPLICATION = 'EB.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'ebdb.sqlite3'),
+try:
+    from .local_settings import DATABASES
+except Exception, e:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'ebdb.sqlite3'),
+        }
     }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
