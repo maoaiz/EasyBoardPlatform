44fb5d463878c568f320bf74f3c69ce520dd9491
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-07-11 10:04:56 -0500

    Docs for celery deamon

--- EB/celery.py
+++ EB/celery.py
@@ -15,7 +15,12 @@ app = Celery('EB')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
+app.conf.update(
+    CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
+)
+app.conf.update(
+    CELERY_RESULT_BACKEND='djcelery.backends.cache:CacheBackend',
+)
 
 @app.task(bind=True)
 def debug_task(self):
